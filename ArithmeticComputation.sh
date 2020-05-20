9f9738b48aa6f55524b9669178ad7fba06a48447
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-20 15:15:09 +0530

    Added Number Read from User

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -2,3 +2,11 @@
 
 
 echo "Welcome to the Arithmetic Computations Program"
+
+
+#---------New Branch-----------------
+ 	read -p "Enter first Number" num1
+   read -p "Enter Second Number" num2
+   read -p "Enter Third Number" num3
+
+	echo "Three Numbers are = " $num1 " " $num2 " " $num3
